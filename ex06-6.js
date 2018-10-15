6cc77a41c88e79ec4d64d082ad0f4949cb0faf23
zm17498 <zm17498@josai.ac.jp>
2018-10-14 22:01:30 -0400

    message

---
+++ ex06-6.js
@@ -0,0 +1,10 @@
+var who66 = 'world';
+function sayhello66() {
+  alert('Hello, '+ who66 + '!');
+}
+function taro66() {
+  alert('太郎がきた');
+}
+function hanako66() {
+  alert('花子がきた');
+}
---
+++ ex06-8.js
@@ -0,0 +1,9 @@
+function sayhello68(){
+  alert('hello,' + document.getElementById('who68').innerHTML + '!')
+}
+function taro68(){
+  document.getElementById('who68').innerHTML = '太郎';
+}
+function taro68(){
+  document.getElementById('who68').innerHTML = '花子';
+}
