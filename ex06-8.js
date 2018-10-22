8de415673c03cff2cd01be42d110cd8542dac889
zm17498 <zm17498@josai.ac.jp>
2018-10-21 21:54:25 -0400

    message

--- ex06-8.js
+++ ex06-8.js
@@ -1,9 +1,9 @@
 function sayhello68(){
-  alert('hello,' + document.getElementById('who68').innerHTML + '!')
+  alert('Hello,' + document.getElementById('who68').innerHTML + '!');
 }
 function taro68(){
   document.getElementById('who68').innerHTML = '太郎';
 }
-function taro68(){
+function hanako68(){
   document.getElementById('who68').innerHTML = '花子';
 }
---
+++ ex06-9.js
@@ -0,0 +1,10 @@
+function txtchange1(){
+  document.getElementById('txt1').innerHTML='ナンバーワン！';
+}
+
+function imgchange1(){
+  document.getElementById('logo').src='./image/aaa.png';
+}
+function imgchange2(){
+  document.getElementById('logo').src='./image/josai.png';
+}
